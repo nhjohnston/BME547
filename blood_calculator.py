e95e7eb09b32f0033b2f7c4c54e25e376a4bd8ac
nhjohnston <88200986+nhjohnston@users.noreply.github.com>
2022-01-12 16:29:56 -0500

    analyze HDL test result for appropriate classification

--- blood_calculator.py
+++ blood_calculator.py
@@ -12,6 +12,16 @@ def interface():
 def accept_input(test_name):
     entry = input("Enter the {} result: ".format(test_name))
     return int(entry)
+
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        answer = "Normal"
+    elif 60 > HDL_value >= 40:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
+    
     
 interface()
 
