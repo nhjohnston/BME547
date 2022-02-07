b67119145794a5f75eca5f3b38bf56c8239eeb03
nhjohnston <88200986+nhjohnston@users.noreply.github.com>
2022-02-07 16:18:03 -0500

    refactored database using dictionaries

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 def add_patient(patient_name, patient_id, age):
-    new_patient = [patient_name, patient_id, age, []]
+    new_patient = {"name": patient_name,
+                   "id": patient_id,
+                   "age": age,
+                   "test": []}
     return new_patient
 
 
@@ -14,15 +17,25 @@ def main():
     db.append(add_patient("David Dinkins", 22, 72))
     found_patient = find_patient(db, 111)
     print(found_patient)
-    print(db)
+    output_database(db)
     add_test_to_patient(db, 111, "HDL", 100)
     print(db)
     return db
 
 
+def output_database(db):
+    for patient in db:
+        output_patient(patient)
+
+
+def output_patient(patient):
+    for key in patient:
+        print("{}: {}".format(key, patient[key]))
+
+
 def find_patient(db, id_no):
     for patient in db:
-        if patient[1] == id_no:
+        if patient["id"] == id_no:
             return patient
     return
 
@@ -30,15 +43,21 @@ def find_patient(db, id_no):
 def add_test_to_patient(db, id_no, test_name, test_result):
     patient = find_patient(db, id_no)
     test_tuple = (test_name, test_result)
-    patient[3].append(test_tuple)
+    patient["test"].append(test_tuple)
 
 
 def print_directory(db):
     rooms = ['Room 13', 'Room 12', 'Room 99', 'Room 3']
     for room, patient in zip(rooms, db):
-        print("Name: {}  Room: {}".format(patient[0], room))
+        print("Name: {}  Room: {}".format(patient["name"], room))
+
+
+def create_id_tag_string(patient):
+    id_string = "{}: {}".format(patient["name"], patient["id"])
+    return id_string
 
 
 if __name__ == "__main__":
     db = main()
     print_directory(db)
+    print(create_id_tag_string(find_patient(db, 111)))
