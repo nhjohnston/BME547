5fecd51524231a60b7ef08a821bff306bf878be9
nhjohnston <88200986+nhjohnston@users.noreply.github.com>
2022-02-28 20:13:49 -0500

    classwork for the past few days

---
+++ client.py
@@ -0,0 +1,27 @@
+import requests
+
+
+#r = requests.get("http://127.0.0.1:5000/")
+#print(r.status_code)
+#print(r.text)
+#
+#
+#r = requests.get("http://127.0.0.1:5000/info")
+#print(r.status_code)
+#print(r.text)
+
+
+out_data = {"name": "Natalie Johnston",
+            "hdl_result": 300}
+r = requests.post("http://127.0.0.1:5000/hdl_check", json = out_data)
+print(r.status_code)
+print(r.text)
+
+
+out_data = {"a": 3,
+            "b": 5}
+r = requests.post("http://127.0.0.1:5000/add", json = out_data)
+print(r.status_code)
+print(r.text)
+a = r.json()
+print(a[0])
---
+++ server.py
@@ -0,0 +1,51 @@
+# server.py
+from flask import Flask, request, jsonify
+from blood_calculator import check_HDL
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def status():
+    return "Server On"
+
+
+@app.route("/info", methods=["GET"])
+def info():
+    out_string = "This is a test server for class.\n"
+    out_string += "This would tell us how to work"
+    return out_string
+
+
+@app.route("/hdl_check", methods=["POST"])
+def check_HDL_from_internet():
+    """
+    input_json = {"name": <patient_name_string>,
+                  "hdl_result": <hdl_int>}
+    """
+    in_data = request.get_json()
+    hdl_value = in_data["hdl_result"]
+    answer = check_HDL(hdl_value)
+    return answer
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    """
+    ["a" : <int>, "b" : <int>,}
+    :return:
+    """
+    in_data = request.get_json()
+    answer = in_data["a"] + in_data["b"]
+    answer_json = [in_data["a"], in_data["b"], answer]
+    return jsonify(answer_json)
+
+
+@app.route("/add/<a>/<b>")
+def add2(a, b):
+    answer = int(a) + int(b)
+    return jsonify(answer)
+
+if __name__ == "__main__":
+    app.run()
+
